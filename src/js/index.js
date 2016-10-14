de7e4f183386e3f773e18f008446fe38e85cf2be
babic <Barbara.Cintoli@fao.org>
2016-10-14 11:29:46 +0200

    Analysis loaded

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,21 @@
 define([
-    "jquery"
-], function ($) {
+    "jquery",
+    "fenix-ui-analysis"
+], function ($, Analysis) {
 
     function App(){
 
-           }
+        this._analysisInit();
+
+    }
+
+    App.prototype._analysisInit = function () {
+
+        var analysis = new Analysis({
+            el: '#fx-analysis-container'
+        });
+    };
+
 
     return App;
-
 });

470d1e2f19163db3fd86b27aeca7f2e83f9e80ce
babic <Barbara.Cintoli@fao.org>
2016-10-17 12:51:36 +0200

    Countries configuration created

--- src/js/index.js
+++ src/js/index.js
@@ -13,15 +13,9 @@ define([
 
     App.prototype._analysisInit = function (COUNTRY_CODE) {
 
-        var analysis = new Analysis({
-            el: '#fx-analysis-container',
-
-            //all components
-            cache : false,
-            environment: "production",
-
-            catalog : AnalysisConfig[COUNTRY_CODE].catalog
-        });
+        var config = AnalysisConfig[COUNTRY_CODE];
+        console.log(config)
+        var analysis = new Analysis(config);
     };
 
     return App;

9d0bd9b12a5e94d781c4384add12425119ff58cb
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2016-11-12 12:29:19 +0100

    Add static initCharts

--- src/js/base.js
+++ src/js/base.js
@@ -1,8 +1,12 @@
-define(function () {
+define([
+    'jquery',
+    'progressbar.js'
+], function ($, ProgressBar) {
 
 
-    function Base(){
+    function Base() {
         this._importThirdPartyCss();
+        this._initCharts();
     }
 
     //style
@@ -15,6 +19,38 @@ define(function () {
         require('../css/countrystat-ui.css');
 
     };
+
+    Base.prototype._initCharts = function () {
+
+        var p = {
+            PERCENTAGE_ONE: '#percentage-one',
+            PERCENTAGE_TWO: '#percentage-two'
+        };
+
+        var c = {
+            color: '#db514b', //color
+            strokeWidth: 10,
+            trailWidth: 9,
+            trailColor: "rgba(255,255,255,0.2)",
+            duration: 1500,
+            easing: 'easeOut',
+            text: {
+                value: '0'
+            },
+            step: function (state, bar) {
+                bar.setText((bar.value() * 100).toFixed(0));
+            }
+        };
+
+        var circle = new ProgressBar.Circle(p.PERCENTAGE_ONE, c);
+
+        circle.animate(0.30);//pOne
+
+        var circle_two = new ProgressBar.Circle(p.PERCENTAGE_TWO, c);
+
+        circle_two.animate(0.70);//pTwo
+
+    }
 
     return new Base();
 });
